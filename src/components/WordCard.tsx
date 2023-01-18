01bf005c1537456c79db160b129d7de5c510c367
ciijay99 <chigozieahanwa99@gmail.com>
2023-01-18 19:34:42 +0100

    commit

--- src/components/WordCard.tsx
+++ src/components/WordCard.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios';
+import axios, { Cancel } from 'axios';
 import { Box, Typography, Card } from '@mui/material';
 
 const WordCard: React.FC = () => {
@@ -10,20 +10,28 @@ const WordCard: React.FC = () => {
   const [word, setWord] = useState<Word>({ word: '', definition: '' });
 
   useEffect(() => {
-    axios
-      .get('https://random-word-api.herokuapp.com/word?number=1')
-      .then((res) => {
-        axios
-          .get(`https://api.dictionaryapi.dev/api/v2/entries/en/${res.data[0]}`)
-          .then((res) => {
-            setWord({
-              word: res.data[0].word,
-              definition: res.data[0].meanings[0].definitions[0].definition,
-            });
-          })
-          .catch((err) => console.log(err));
-      })
-      .catch((err) => console.log(err));
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(
+          'https://random-word-api.herokuapp.com/word?number=1'
+        );
+        const data = await axios.get(
+          `https://api.dictionaryapi.dev/api/v2/entries/en/${res.data[0]}`
+        );
+        setWord({
+          word: data.data[0].word,
+          definition: data.data[0].meanings[0].definitions[0].definition,
+        });
+      } catch (err) {
+        if (err.response.status === 404) {
+          console.log('word not found');
+          window.location.reload();
+        } else {
+          console.log(err);
+        }
+      }
+    };
+    fetchData();
   }, []);
 
   const capitalized = word.word.charAt(0).toUpperCase() + word.word.slice(1);
